d007f86a0f21bbda70c26e679d9384fad0007ca3
Michael Caesario <michaelcaesario@live.com>
2022-05-04 18:26:52 +0700

    Initial Commit

---
+++ ExampleSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExampleSwiftUI
+//
+//  Created by Michael Caesario on 04/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExampleSwiftUI/ExampleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleSwiftUIApp.swift
+//  ExampleSwiftUI
+//
+//  Created by Michael Caesario on 04/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ExampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
