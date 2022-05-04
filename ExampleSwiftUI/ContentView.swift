426b9b743298861d063af0b4b0d7a9f83920e521
Michael Caesario <michaelcaesario@live.com>
2022-05-04 20:46:51 +0700

    added textfield, button and a text to the view

--- ExampleSwiftUI/ContentView.swift
+++ ExampleSwiftUI/ContentView.swift
@@ -8,11 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: - Properties
+    
+    // binding
+    @State private var textfield: String = ""
+    
+    //MARK: - View
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // Stack the view vertically with VStack with center alignment
+        VStack(alignment: .center) {
+            
+            // Add empty space to push the view in between
+            Spacer()
+            
+            // Text to show the content inside a textfield
+            Text(textfield)
+                .font(.title)
+                .fontWeight(.bold)
+            
+            // Add empty space to push the view in between
+            Spacer()
+            
+            // Textfield
+            TextField("Enter your name here", text: $textfield)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.blue.opacity(0.2))
+                )
+                .padding()
+            
+            // Button to save the text from the textfield
+            Button {
+                // More code here
+            } label: {
+                Text("Submit")
+                    .font(.title3)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .fill(Color.blue)
+                    )
+                    .padding()
+            }
+        }
     }
 }
+
+//MARK: - SwiftUI Previews
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
